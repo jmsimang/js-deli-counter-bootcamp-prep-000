6d69e841d6dc171fae11ce11422153e9e2af4c5a
Jabulani Msimango <msimango.jabulani@moov.life>
2018-01-15 15:27:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function takeANumber(line, name) {
 
 function nowServing(line) {
   if (line.length == 0)
-    return "There is nobody in line"
+    return "There is nobody waiting to be served!"
   var customer = ""
   customer += line[0]
   line.shift()
