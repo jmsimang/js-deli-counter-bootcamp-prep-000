ff5df4cb3bfebc506ba45372c4138556dfd2e663
Jabulani Msimango <msimango.jabulani@moov.life>
2018-01-15 15:32:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function takeANumber(line, name) {
   line.push(name)
   var lineNumber = line.length
-  return "Welcome, ${name}. You are number ${lineNumber} in line."
+  return `Welcome, ${name}. You are number ${lineNumber} in line.`
 }
 
 function nowServing(line) {
@@ -10,7 +10,7 @@ function nowServing(line) {
   var customer = ""
   customer += line[0]
   line.shift()
-  return 'Currently serving '+customer+'.'
+  return `Currently serving ${customer}.`
 }
 
 function currentLine(line) {
