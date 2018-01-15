82c538a2599a843ff87f590d8d491aeec1b267dd
Jabulani Msimango <msimango.jabulani@moov.life>
2018-01-15 15:37:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function nowServing(line) {
 function currentLine(line) {
   if (line.length == 0)
     return "The line is currently empty."
-  var message = "The line is currently"
-  for (let i = 1; i<=line.length; i++) {
-    message += `${i}. ${line[i]}`
+  var message = "The line is currently: "
+  for (let i = 0; i<line.length; i++) {
+    message += `${i} +1. ${line[i]}`
   }
   return message
 }
