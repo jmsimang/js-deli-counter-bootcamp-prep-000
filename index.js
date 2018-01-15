d2f1011235caf0706285f1a48d089a5d76dfd092
Jabulani Msimango <msimango.jabulani@moov.life>
2018-01-15 15:26:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ function takeANumber(line, name) {
 }
 
 function nowServing(line) {
-  
+  if (line.length() == 0)
+    return "There is nobody in line"
+  var customer = ""
+  customer += line[0]
+  line.shift()
+  return "Currently serving ${customer}"
 }
 
 function currentLine(line) {
